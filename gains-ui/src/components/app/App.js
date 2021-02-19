7a3e1a4433ebadc95f732177fea5f1221938d593
johnny-nepp <79328605+johnny-nepp@users.noreply.github.com>
2021-02-19 15:58:32 -0600

    Add in swol gains calendar on app

--- gains-ui/src/components/app/App.js
+++ gains-ui/src/components/app/App.js
@@ -1,15 +1,18 @@
-// import './App.css';
+import './App.css';
 
 function App() {
   return (
-      <>
+    <>
       <h1 >Swol Gains</h1>
       <button>General Muscle Gain</button>
       <button>Flexibility</button>
       <button>Aerobic Stamina</button>
       <button>Higher Energy</button>
       <button>Targeted Muscle Groups</button>
-      </>
+      <div>
+        <iframe src="https://calendar.google.com/calendar/embed?src=c_cicgu662hcmbk5jd5fi73dmkik%40group.calendar.google.com&ctz=America%2FChicago" />
+      </div>
+    </>
   );
 }
 
