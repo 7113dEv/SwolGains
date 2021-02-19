df44d140df8c1f4c8701ccd66d462fa4ee61b22d
Sean Tunell <stunell@catalyte.io>
2021-02-19 13:54:13 -0700

    added buttons, works

--- gains-ui/src/components/app/App.js
+++ gains-ui/src/components/app/App.js
@@ -1,10 +1,15 @@
-import './App.css';
+// import './App.css';
 
 function App() {
   return (
-    <div>
-
-    </div>
+      <>
+      <h1 >Swol Gains</h1>
+      <button>General Muscle Gain</button>
+      <button>Flexibility</button>
+      <button>Aerobic Stamina</button>
+      <button>Higher Energy</button>
+      <button>Targeted Muscle Groups</button>
+      </>
   );
 }
 
--- gains-ui/src/index.js
+++ gains-ui/src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from '.components/app/App';
+import App from './components/app/App.js';
 
 ReactDOM.render(
   <React.StrictMode>
