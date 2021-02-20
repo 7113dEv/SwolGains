b83450ee2ead3be785d777c5d7b84f1093646500
Giovanni Maio <gmaio@catalyte.io>
2021-02-19 17:49:46 -0700

    Add back-end

---
+++ swolgains-api/src/main/java/api/swolgains/SwolGainsApiApplication.java
@@ -0,0 +1,12 @@
+package api.swolgains;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwolGainsApiApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SwolGainsApiApplication.class, args);
+  }
+}
