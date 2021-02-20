9fd7736341eb98fa68f78b99b1c33c9a0d2585bf
Giovanni Maio <gmaio@catalyte.io>
2021-02-19 17:47:10 -0700

    Sending to repo

--- gains-ui/src/components/app/App.js
+++ gains-ui/src/components/app/App.js
@@ -1,6 +1,15 @@
+import React from 'react';
+import GoogleLogin from 'react-google-login';
+import constants from '../../constants';
+
 import './App.css';
 
-function App() {
+function App() {  
+
+  const responseGoogle = (response) => {
+    console.log(response);
+  }
+
   return (
     <>
       <h1 >Swol Gains</h1>
@@ -9,6 +18,13 @@ function App() {
       <button>Aerobic Stamina</button>
       <button>Higher Energy</button>
       <button>Targeted Muscle Groups</button>
+      <GoogleLogin
+        clientId={constants.OAUTH_CLIENT_ID}
+        buttonText="Login"
+        onSuccess={responseGoogle}
+        onFailure={responseGoogle}
+        cookiePolicy={'single_host_origin'}
+      />
       <div>
         <iframe src="https://calendar.google.com/calendar/embed?src=c_cicgu662hcmbk5jd5fi73dmkik%40group.calendar.google.com&ctz=America%2FChicago" />
       </div>
