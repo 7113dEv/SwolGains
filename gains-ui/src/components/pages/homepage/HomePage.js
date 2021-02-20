7b51ece5d7bff4ab96f93c7c4c2fe5c678fd1b4a
Giovanni Maio <gmaio@catalyte.io>
2021-02-19 19:22:35 -0700

    Fix page

--- gains-ui/src/components/pages/homepage/HomePage.js
+++ gains-ui/src/components/pages/homepage/HomePage.js
@@ -1,22 +1,9 @@
 import React from 'react'
-import GoogleLogin from 'react-google-login';
-import constants from '../../../utilities/constants';
 
 const HomePage = () => {
-  const responseGoogle = (response) => {
-    sessionStorage.setItem("auth", response);
-  }
-
   return (
     <div>
       <h1 >Swol Gains</h1>
-      <GoogleLogin
-        clientId={constants.OAUTH_CLIENT_ID}
-        buttonText="Login"
-        onSuccess={responseGoogle}
-        onFailure={responseGoogle}
-        cookiePolicy={'single_host_origin'}
-      />
       <div>
         <iframe src="https://calendar.google.com/calendar/embed?src=c_cicgu662hcmbk5jd5fi73dmkik%40group.calendar.google.com&ctz=America%2FChicago" />
       </div>
