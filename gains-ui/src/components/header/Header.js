1da2d23a2e8bd486cc357b9422cea1f006a8e05a
Giovanni Maio <gmaio@catalyte.io>
2021-02-19 20:43:27 -0700

    Logout button is on header but doesn't do shit

--- gains-ui/src/components/header/Header.js
+++ gains-ui/src/components/header/Header.js
@@ -5,11 +5,14 @@ import styles from './header.module.css'
 const Header = () => {
   return (
     <div className={styles.header}>
-      <NavLink to="/general-muscle-gain" className={styles.navbutton}>General Muscle Gain</NavLink>
-      <NavLink to="/flexibility" className={styles.navbutton}>Flexibility</NavLink>
-      <NavLink to="/aerobic-stamina" className={styles.navbutton}>Aerobic Stamina</NavLink>
-      <NavLink to="/higher-energy" className={styles.navbutton}>Higher Energy</NavLink>
-      <NavLink to="/targeted-muscle-groups" className={styles.navbutton}>Targeted Muscle Groups</NavLink>
+        <NavLink to="/general-muscle-gain" className={styles.navbutton}>General Muscle Gain</NavLink>
+        <NavLink to="/flexibility" className={styles.navbutton}>Flexibility</NavLink>
+        <NavLink to="/aerobic-stamina" className={styles.navbutton}>Aerobic Stamina</NavLink>
+        <NavLink to="/higher-energy" className={styles.navbutton}>Higher Energy</NavLink>
+        <NavLink to="/targeted-muscle-groups" className={styles.navbutton}>Targeted Muscle Groups</NavLink>
+      <div className={styles.logout}> 
+        <button>Logout</button>
+      </div>
     </div>
   );
 }
