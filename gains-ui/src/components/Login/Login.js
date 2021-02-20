fd0bd485f24204df6b3e6494f6957980b94f6664
Giovanni Maio <gmaio@catalyte.io>
2021-02-19 18:27:02 -0700

    commit for merge

---
+++ gains-ui/src/components/Login/Login.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import GoogleLogin from 'react-google-login';
+
+export const Login = () => {
+  return (
+    <div>
+      <h1>
+        Login Page
+      </h1>
+    </div>
+  )
+}
--- gains-ui/src/components/app/App.js
+++ gains-ui/src/components/app/App.js
@@ -1,34 +1,40 @@
-import React from 'react';
-import GoogleLogin from 'react-google-login';
-import constants from '../../constants';
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Login from '../Login';
+import constants from '../../utilities/constants';
 
 import './App.css';
 
 function App() {
-
-  const responseGoogle = (response) => {
-    console.log(response);
-  }
+  const [loggedIn, setLoggedIn] = useState(localStorage.getItem('auth'));
 
   return (
-    <>
-      <h1 >Swol Gains</h1>
-      <button>General Muscle Gain</button>
-      <button>Flexibility</button>
-      <button>Aerobic Stamina</button>
-      <button>Higher Energy</button>
-      <button>Targeted Muscle Groups</button>
-      <GoogleLogin
-        clientId={constants.OAUTH_CLIENT_ID}
-        buttonText="Login"
-        onSuccess={responseGoogle}
-        onFailure={responseGoogle}
-        cookiePolicy={'single_host_origin'}
-      />
-      <div>
-        <iframe src="https://calendar.google.com/calendar/embed?src=c_cicgu662hcmbk5jd5fi73dmkik%40group.calendar.google.com&ctz=America%2FChicago" />
+    <Router>
+      <div className="site-main-block">
+        <Route
+          path="/"
+          render={(props) =>
+            props.location.pathname !== constants.LOGIN_PATH && (
+              <p></p>
+              // <Header className="App-header" setLoggedOut={setLoggedOut} />
+            )
+          }
+        />
+        <Switch>
+          <Route
+            exact
+            path={constants.LOGIN_PATH}
+            component={() => (
+              <Login
+                setIsLoggedIn={setIsLoggedIn}
+                loggedOut={loggedOut}
+                setLoggedOut={setLoggedOut}
+              />
+            )}
+          />
+        </Switch>
       </div>
-    </>
+    </Router>
   );
 }
 
